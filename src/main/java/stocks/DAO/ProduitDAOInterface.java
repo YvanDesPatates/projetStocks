d55ddb50bacfbc4a5baeb22056a80d1621188c79
roux <yvan.roux@inrae.fr>
2023-04-30 11:47:11 +0200

    [partie 3] couche d'accès auc donnée DAO

--- src/main/java/stocks/DAO/ProduitDAOInterface.java
+++ src/main/java/stocks/DAO/ProduitDAOInterface.java
@@ -5,8 +5,8 @@ import stocks.metier.produit.I_Produit;
 import java.util.List;
 
 public interface ProduitDAOInterface {
-    List<I_Produit> getAll();
     boolean create(I_Produit produit);
     boolean delete(I_Produit produit);
     boolean update(I_Produit produit);
+    List<I_Produit> getAllFromCatalogue(String nomCatalogue);
 }
